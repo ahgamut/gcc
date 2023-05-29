cc4243591cb289af7743cf5f0a3ddb6ad6012c97
Gautham <ahgamut@gmail.com>
2023-05-29 15:20:38 -0500

    created separate files for portcosmo
    if I create a separate folder, it crashes because the build system
    doesn't create the .deps folder.

--- gcc/c-family/c-common.c
+++ gcc/c-family/c-common.c
@@ -51,7 +51,7 @@ along with GCC; see the file COPYING3.  If not see
 #include "c-spellcheck.h"
 #include "selftest.h"
 #include "debug.h"
-#include "print-tree.h"
+#include "c-family/portcosmo.h"
 
 cpp_reader *parse_in;		/* Declared in c-pragma.h.  */
 
@@ -2114,8 +2114,7 @@ check_case_value (location_t loc, tree value)
   else if (value != error_mark_node)
     {
       if (flag_portcosmo) {
-          inform(loc, "we're here\n");
-          debug_tree(value);
+          portcosmo_show_tree(loc, value);
       }
       error_at (loc, "case label does not reduce to an integer constant");
       value = error_mark_node;
---
+++ gcc/c-family/portcosmo.h
@@ -0,0 +1,21 @@
+#ifndef PORTCOSMO_H
+#define PORTCOSMO_H
+/* first stdlib headers */
+#include <stdio.h>
+/* now all the plugin headers */
+#include <gcc-plugin.h>
+/* first gcc-plugin, then the others */
+#include <c-family/c-common.h>
+#include <c-family/c-pragma.h>
+#include <c/c-tree.h>
+#include <cgraph.h>
+#include <cpplib.h>
+#include <diagnostic.h>
+#include <print-tree.h>
+#include <stringpool.h>
+#include <tree-iterator.h>
+#include <tree.h>
+
+void portcosmo_show_tree(location_t, tree);
+
+#endif /* PORTCOSMO_H */
